8278247c19c529999c7635904f6dd13b9f88afd3
Josef Hak <jhak@redhat.com>
2019-09-06 13:42:49 +0200

    updated docker-compose example

--- dev/db/inventory.sql
+++ dev/db/inventory.sql
@@ -3,9 +3,11 @@ CREATE TABLE public.hosts (
     account character varying(10),
     metadata jsonb,
     logs jsonb,
+    tags character varying(200),
     PRIMARY KEY (id)
 );
 
-INSERT INTO public.hosts VALUES (1, '12234', '{"country":"USA"}', '{"info":"running"}');
-INSERT INTO public.hosts VALUES (2, '34634', '{"address":{"code": 555}, "a":1}', '{"debug":"printed"}');
-INSERT INTO public.hosts VALUES (3, '34523', '{"address":{"city":"Studenec"},"add":0}', '{"error":"null pointer"}');
+INSERT INTO public.hosts VALUES (1, '12234', '{"country":"USA"}', NULL, '["tag1", "tag2"]');
+INSERT INTO public.hosts VALUES (2, '34634', NULL, '{"debug":"printed"}', NULL);
+INSERT INTO public.hosts VALUES (3, '34523', '{"address":{"city":"Studenec"}}', '{}', '[]');
+INSERT INTO public.hosts VALUES (4, '23423', '{"address":{"city":"Studenec"}}', NULL, '[malformated]');
